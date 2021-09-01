9cea2205b8780f783543bdfa960f37fb117d302d
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-01 14:22:39 +0200

    declare the instance variables

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <!-- 
--è definita una classe ‘Movie’ => all'interno della classe sono dichiarate delle variabili d'istanza => all'interno della classe è definito un costruttore => all'interno della classe è definito almeno un metodo
+-è definita una classe ‘Movie’ 
+  => all'interno della classe sono dichiarate delle variabili d'istanza (o oggetti)
+  => all'interno della classe è definito un costruttore 
+  => all'interno della classe è definito almeno un metodo
 
 -vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà 
 -->
+
+<?php
+
+class Movie {
+  public $title;
+  public $genre;
+  public $year;
+  public $length = 0;
+}
+
+$fight_club = new Movie();
+$memento = new Movie();
+$ritorno_al_futuro = new Movie();
+
