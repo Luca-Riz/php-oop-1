d035d4639ab4d0a535b6931e100b2bc2b88e44be
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-01 15:44:01 +0200

    add athers films

--- index.php
+++ index.php
@@ -16,13 +16,16 @@ class Movie {
   public $length = 0;
   public $old = 'Questo film é recente';
 
-  function __construct($_title){
+  function __construct($_title, $_year){
     $this->title = $_title;
+    $this->year = $_year;
+
+    $this->setOld();
   }
 
   public function setOld(){
-    if($this->year < 1980){
-      $this->old = "Questo film risale a prima del 1980";
+    if($this->year < 1990){
+      $this->old = "Questo film risale a prima del 1990";
     }
   }
 
@@ -31,15 +34,26 @@ class Movie {
   }
 }
 
-$fight_club = new Movie('Fight club');
+// Fight club
+$fight_club = new Movie('Fight club',1999);
 $fight_club->genre = "Drama";
-$fight_club->year = 1960;
 $fight_club->length = 139;
+$old = $fight_club->getOld();
 var_dump($fight_club);
 
-$class_methods = get_class_methods('Movie');
-var_dump($class_methods);
+// Memento
+$memento = new Movie('Memento',2000);
+$memento->genre = "Mystery, Thriller";
+$memento->length = 113;
+$old = $memento->getOld();
+var_dump($memento);
 
-// $memento = new Movie();
-// $ritorno_al_futuro = new Movie();
+// Ritorno al futuro
+$ritorno_al_futuro = new Movie('Ritorno al futuro', 1985);
+$ritorno_al_futuro->genre = "Adventure, Comedy, Sci-Fi";
+$ritorno_al_futuro->length = 116;
+$old = $ritorno_al_futuro->getOld();
+var_dump($ritorno_al_futuro);
 
+// $class_methods = get_class_methods('Movie');
+// var_dump($class_methods);
